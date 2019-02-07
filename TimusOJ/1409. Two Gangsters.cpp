64a2d7f8b1f78dd1bbfc5ac50bdfba207846e0c6
Md Jubel Hossain <19242508+AlosNumberOne@users.noreply.github.com>
2019-02-08 00:08:37 +0600

    Create 1409. Two Gangsters.cpp

---
+++ TimusOJ/1409. Two Gangsters.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b;
+	scanf("%d%d",&a,&b);
+	int result1=b-1;
+	int result2=a-1;
+	printf("%d %d\n", result1 , result2);
+	return 0;
+}
