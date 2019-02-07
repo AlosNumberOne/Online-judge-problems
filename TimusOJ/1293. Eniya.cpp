bfd9dc6c9a2dae6c6e54f558715417f761fb6b1c
Md Jubel Hossain <19242508+AlosNumberOne@users.noreply.github.com>
2019-02-08 00:07:32 +0600

    Create 1293. Eniya.cpp

---
+++ TimusOJ/1293. Eniya.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	 int n,a,b;
+             scanf("%d%d%d",&n,&a,&b);
+             int result=n*a*b*2;
+             printf("%d\n",result);
+	return 0;
+}
